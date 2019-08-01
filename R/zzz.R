2a18d7a258804881ab04a98c96ce82798afef0ab
Anastasis Giannousakis <giannou@pik-potsdam.de>
2019-08-01 15:53:51 +0200

    Add files via upload

---
+++ R/zzz.R
@@ -0,0 +1,4 @@
+.onAttach <- function(libname,pkgname) {
+ warning('This is just an auxilliary package that allows for installing all MO packages at once. Please only use it for this purpose
+          and do not use it in your scripts!', call.=FALSE)
+}
